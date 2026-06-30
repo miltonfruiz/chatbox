a841498bdaf65d0911aaadaf9ed2bac25a9f19da
Milton Franco Ruiz <94766039+miltonfruiz@users.noreply.github.com>
2026-06-30 04:16:59 -0300

    Add backend/server.js

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const mongoUri = process.env.MONGO_URI;
+
+mongoose.connect(mongoUri)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log('Error connecting to MongoDB: ' + err));
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+
+app.use(limiter);
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.listen(port, () => {
+  console.log('Server is listening on port ' + port);
+});
